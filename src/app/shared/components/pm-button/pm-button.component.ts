e881fb872bbcecbbded4d4b0b83776f0aefa5881
sen-sourabh <sourabhsen201313@gmail.com>
2024-08-17 20:57:50 +0530

    Updates: New Packages and file creations

---
+++ src/app/shared/components/pm-button/pm-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-pm-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './pm-button.component.html',
+  styleUrl: './pm-button.component.scss',
+})
+export class PmButtonComponent {}
