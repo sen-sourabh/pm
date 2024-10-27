7744d49f1f0a9eba0c5ec4e8a5abfef8cd32e2a3
sen-sourabh <sourabhsen201313@gmail.com>
2024-10-27 12:25:12 +0530

    Angular sidebar and side content fixes with blue theme

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,31 +1,11 @@
-import { CommonModule, NgIf } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { CoreModule } from '../../../core/core.module';
 import { SidebarMenus } from '../../helpers/layout';
-import { LayoutRoutingModule } from '../../layout-routing.module';
 import { SidebarMenusType } from '../../types';
-import { FooterComponent } from "../footer/footer.component";
-import { HeaderComponent } from "../header/header.component";
-import { SidenavContentComponent } from "../sidenav-content/sidenav-content.component";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    CoreModule,
-    NgIf,
-    LayoutRoutingModule,
-    MatSidenavModule,
-    MatIconModule,
-    SidenavContentComponent,
-    FooterComponent,
-    HeaderComponent,
-  ],
 })
 export class SidebarComponent {
   @ViewChild('sidenav') sidenav: any;
